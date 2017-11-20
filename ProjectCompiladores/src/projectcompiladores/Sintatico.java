e63aa83b62397f5ebc7f22f5a15fe26b0bb376ff
rocha <rocha@DESKTOP-V7UN895>
2017-11-20 04:11:16 -0300

    Sintatico run

--- ProjectCompiladores/src/projectcompiladores/Sintatico.java
+++ ProjectCompiladores/src/projectcompiladores/Sintatico.java
@@ -15,9 +15,8 @@ public class Sintatico {
 	public void texto(){
 			
 		if(sentenca()){
-			System.out.println(palavras.get(i).getPalavra());
                         
-			if(palavras.get(i).getPalavra().equals(".")){
+			if(pontoFinal()){
 				System.out.println("Rodou");
 			}else{
 				System.out.println("Falta ponto final");
@@ -28,6 +27,10 @@ public class Sintatico {
 	
 	public boolean sentenca(){
                 if(sintagmaNominal()){
+                    return true;
+                }
+                i = 0;
+                if(proAdj())
                     return true;
                     /**if(SintagmaVerbal()){
             		return true;
@@ -46,7 +49,7 @@ public class Sintatico {
             
             
                 
-        }
+        
                 return false;
         }
         public boolean sintagmaNominal(){
@@ -86,93 +89,11 @@ public class Sintatico {
       
             return false;
         }
-        /**
-        public boolean SintagmaVerbal(){
-        	
-        	if(encontraClassifi("verbo")){
-        		return true;
-        	}else if(encontraClassifi("verbo") && SintagmaPrep()){
-        		return true;
-        	}
-        	
-			return false;
-        	
-        }
         
-        public boolean SintagmaPrep(){
-        	return false;
-        }
-        
-        public boolean SintagmaAdver(){
-        	
-        	if(adverbio()){
-        		SintagmaAdver2();
-        	}else {
-                    
-        	}
-                return true;
-        }
-        
-        public boolean SintagmaAdver2(){
-        	return false;
-        }
-        
-        public boolean SintagmaAdje(){
-        	return false;
-        }
-        
-        public boolean adverbio(){
-        	
-        	if(encontraClassifi("advérbio")){
-        		i++;
-        		adverbio2();
-        		return true;
-        	}
-        	return false;
-        }
-        
-        public boolean adverbio2(){
-        	
-        	if(SintagmaPrep());{
-        		i++;
-        		adverbio2();
-        		return true;
-        		
-        	}
-        	
-        	
-        }
-        
-        public boolean substantivo(){
-        	
-        	if(encontraClassifi("pronome")){
-        		i++;
-        		substantivo2();
-        		return true;
-        	}else if(encontraClassifi("substantivo")){
-        		i++;
-        		substantivo2();
-        		return true;
-        	}
-        	
-        	return false;
-        }
-        
-        public boolean substantivo2(){
-        	
-        	if(SintagmaAdje()){
-        		substantivo();
-        		substantivo2();
-        		return true;
-        	}
-        	return false;
-        	
-        }
-        */
         /** Verifica se a palavra atual tem a classificacao passada como parametro*/
         public boolean encontraClassifi(String nome){
         	
-        	for(int j = 0; j < palavras.size(); j++)
+        	for(int j = 0; j < palavras.get(i).getClassificacao().size(); j++)
         	{
         	    if(palavras.get(i).getClassificacao().get(j).equals(nome)){
         	    	return true;
@@ -186,7 +107,34 @@ public class Sintatico {
         public void incrementofinal(){
                 if(i < palavras.size() - 1)
                     i++;              
-       }
+        }
+        
+        public boolean proAdj(){
+        	
+        	if(encontraClassifi("pronome") || encontraClassifi("pronome demonstrativo")){
+        		i++;
+                        
+        		if(encontraClassifi("substantivo masculino")){
+        			
+                                incrementofinal();
+                                
+        			return true;
+        		}else if(encontraClassifi("substantivo feminino")){
+        			
+                                incrementofinal();
+                                
+        			return true;
+        		}
+        		
+        		return false;
+        	}
+        	
+        	return false;
+        }
+        
+        public boolean pontoFinal(){
+            return palavras.get(i).getPalavra().equals(".") || palavras.get(i).getPalavra().equals("!") || palavras.get(i).getPalavra().equals("?");
+        }
 
 
 }
