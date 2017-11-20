d96e32b674ba7b9a6e6807538f65eb3413f4b4e3
rocha <rocha@DESKTOP-V7UN895>
2017-11-20 01:15:09 -0300

    Commit do sintatico

--- ProjectCompiladores/src/projectcompiladores/Sintatico.java
+++ ProjectCompiladores/src/projectcompiladores/Sintatico.java
@@ -6,44 +6,153 @@ public class Sintatico {
     
         int i = 0;
         ArrayList<ClassificaPalavra> palavras = new ArrayList<ClassificaPalavra>();
-	public Sintatico(ArrayList<ClassificaPalavra> palavras ){
-            this.palavras = palavras;
-	}
+		
+        public Sintatico(ArrayList<ClassificaPalavra> palavras ){
+	            this.palavras = palavras;
+		}
+	        
         
-        public boolean sentenca(ArrayList<ClassificaPalavra> palavras){
-            if(isSintagmaNominal()){
-                System.out.println("É SINTAGMA NOMINAL");
-                return true;
+		public void texto(){
+			
+			if(sentenca()){
+								
+				if(palavras.get(i).getPalavra().equals(".")){
+					System.out.println("Rodou");
+				}else{
+					System.out.println("Falta ponto final");
+				}
+				
+			}
+		}
+	
+		public boolean sentenca(){
+            if(SintagmaNominal() ){
+            	i++;
+            	if(SintagmaVerbal()){
+            		return true;
+            	}
+            	
+            }else if(SintagmaVerbal()){
+            	i++;
+            	if(SintagmaNominal()){
+            		return true;
+            	}
+            	
+            }if(SintagmaAdver()){
+            	i++;
+            	sentenca();
             }
+            
+            
             return false;
         }
         
-        public boolean isSintagmaNominal(){
-            for(int j = 0; j < palavras.get(i).getClassificacao().size(); j++){
-                if(palavras.get(i).getClassificacao().get(j).equals("artigo definido")){
-                    i++;
-                    if(palavras.get(i).getClassificacao().get(j).equals("substantivo masculino"))
-                        return true;
-                    else if(palavras.get(i).getClassificacao().get(j).equals("substantivo feminino"))
-                        return true;
-                }
-                else if(palavras.get(i).getClassificacao().get(j).equals("artigo indefinido")){
-                    i++;
-                    if(palavras.get(i).getClassificacao().get(j).equals("substantivo masculino"))
-                        return true;
-                    else if(palavras.get(i).getClassificacao().get(j).equals("substantivo feminino"))
-                        return true;
-                }
-                else if(palavras.get(i).getClassificacao().get(j).equals("numeral")){
-                    i++;
-                    i += 1;
-                    if(palavras.get(i).getClassificacao().get(j).equals("substantivo masculino"))
-                        return true;
-                    else if(palavras.get(i).getClassificacao().get(j).equals("substantivo feminino"))
-                        return true;
-                }
+        public boolean SintagmaNominal(){
+            if(palavras.get(i).getClassificacao().equals("artigo definido")){
+                i++;
+                if(palavras.get(i).getClassificacao().equals("substantivo masculino"))
+                    return true;
+                else if(palavras.get(i).getClassificacao().equals("substantivo feminino"))
+                    return true;
+            }
+            else if(palavras.get(i).getClassificacao().equals("artigo indefinido")){
+                i++;
+                if(palavras.get(i).getClassificacao().equals("substantivo masculino"))
+                    return true;
+                else if(palavras.get(i).getClassificacao().equals("substantivo feminino"))
+                    return true;
+            }
+            else if(palavras.get(i).getClassificacao().equals("numeral")){
+                i++;
+                i += 1;
+                if(palavras.get(i).getClassificacao().equals("substantivo masculino"))
+                    return true;
+                else if(palavras.get(i).getClassificacao().equals("substantivo feminino"))
+                    return true;
             }
       
             return false;
+        }
+ 
+        public boolean SintagmaVerbal(){
+        	
+        	if(palavras.get(i).getClassificacao().equals("verbo")){
+        		return true;
+        	}else if(palavras.get(i).getClassificacao().equals("verbo") && SintagmaPrep()){
+        		return true;
+        	}
+        	
+			return false;
+        	
+        }
+        
+        public boolean SintagmaPrep(){
+        	return false;
+        }
+        
+        public boolean SintagmaAdver(){
+        	
+        	if(adverbio()){
+        		SintagmaAdver2();
+        	}else {
+                    
+        	}
+                return true;
+        }
+        
+        public boolean SintagmaAdver2(){
+        	return false;
+        }
+        
+        public boolean SintagmaAdje(){
+        	return false;
+        }
+        
+        public boolean adverbio(){
+        	
+        	if(palavras.get(i).getClassificacao().equals("advérbio")){
+        		i++;
+        		adverbio2();
+        		return true;
+        	}
+        	return false;
+        }
+        
+        public boolean adverbio2(){
+        	
+        	if(SintagmaPrep());{
+        		i++;
+        		adverbio2();
+        		return true;
+        		
+        	}
+        	
+        	
+        }
+        
+        public boolean substantivo(){
+        	
+        	if(palavras.get(i).getClassificacao().equals("pronome")){
+        		i++;
+        		substantivo2();
+        		return true;
+        	}else if(palavras.get(i).getClassificacao().equals("substantivo")){
+        		i++;
+        		substantivo2();
+        		return true;
+        	}
+        	
+        	return false;
+        }
+        
+        public boolean substantivo2(){
+        	
+        	if(SintagmaAdje()){
+        		substantivo();
+        		substantivo2();
+        		return true;
+        	}
+        	return false;
+        	
         }
 }
